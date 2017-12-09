cb60b0cb5ee84ca4d5e92e5f9070ee5872cb1db5
monado <monado@xenoblade.cn>
2017-12-09 12:59:05 +0800

    add semantic plugin

---
+++ src/lib/plugins/semantic/conf/default.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Default settings for the semantic plugin
+ *
+ * @author Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ */
+
+$conf['useJSONLD']            = 1;
+$conf['defaultSchemaOrgType'] = 'Article';
+$conf['exposeWebService']     = 1;
+$conf['useMetaDescription']   = 1;
+$conf['useMetaAuthor']        = 1;
+$conf['useDublinCore']        = 0;
+$conf['useOpenGraph']         = 0;
+$conf['excludedPages']        = '(wiki|playground)';
