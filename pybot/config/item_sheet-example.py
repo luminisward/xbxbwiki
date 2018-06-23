09c19baca2e7ef19ba47fad8e04aabde984f3b5b
monado <monado@xenoblade.cn>
2018-06-23 17:52:16 +0800

    add items scripts file

---
+++ pybot/config/item_sheet-example.py
@@ -0,0 +1,8 @@
+DATA_SOURCE = '' # 'CSV' or 'GoogleSheets'
+
+# CSV
+CSV_PATH = ''
+
+# google sheets settings, see https://developers.google.com/sheets/api/guides/concepts
+SPREADSHEET_ID = '' 
+RANGE_NAME = ''
