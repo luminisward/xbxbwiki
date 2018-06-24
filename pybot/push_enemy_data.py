3c083319a1ed983b0a95dae2244f3a35dbe33736
luminisward <11269982+luminisward@users.noreply.github.com>
2018-06-24 16:20:07 +0800

    Item script (#10)
    * change folder structure
    
    * add items scripts file
    
    * update item script
    
    * change folder structure
    
    * add items scripts file
    
    * update item script
    
    * remove init import
    
    * filter empty data

--- pybot/push_enemy_data.py
+++ pybot/push_enemy_data.py
@@ -1,7 +1,7 @@
 import sys
 import dokuwiki
-from google_sheets import GoogleSheets
-import enemy
+from lib.google_sheets import GoogleSheets
+import lib.enemy as enemy
 
 def push_enemy_data(data_list, wiki):
     for data_dict in data_list:
@@ -27,26 +27,27 @@ def push_enemy_data(data_list, wiki):
 
 if __name__ == '__main__':
     try:
-        import config
+        from config.mywiki import *
+        from config.enemy_sheet import *
     except ModuleNotFoundError:
         print('找不到config')
         sys.exit()
 
     try:
-        wiki = dokuwiki.DokuWiki(config.siteurl, config.username, config.password, True)
+        wiki = dokuwiki.DokuWiki(siteurl, username, password, True)
     except dokuwiki.DokuWikiError:
         print('Username or password is wrong ,can\'t access wiki')
         sys.exit()
 
-    if config.DATA_SOURCE == 'CSV':
+    if DATA_SOURCE == 'CSV':
         # read CSV file
-        data_table = enemy.EnemyCSV(config.CSV_PATH)
+        data_table = enemy.EnemyCSV(CSV_PATH)
         dict_list = data_table.get_data()
-    elif config.DATA_SOURCE == 'GoogleSheets':
+    elif DATA_SOURCE == 'GoogleSheets':
         # Set sheet instance
         enemy_sheet = GoogleSheets()
-        enemy_sheet.sheet_id = config.SPREADSHEET_ID
-        enemy_sheet.range = config.RANGE_NAME
+        enemy_sheet.sheet_id = SPREADSHEET_ID
+        enemy_sheet.range = RANGE_NAME
         # pull data
         enemy_sheet.pull_data()
         dict_list = enemy_sheet.dict_list
---
+++ pybot/push_item_data.py
@@ -0,0 +1,43 @@
+import sys
+import dokuwiki
+from lib.google_sheets import GoogleSheets
+import lib.item as item
+
+def push_item_data(data_list, wiki):
+    for data_dict in data_list:
+        try:
+            if data_dict['简中'] == '':
+                continue
+            print(data_dict['简中'])
+            path = '物品:' + data_dict['简中']
+            wikitext = item.ItemPageBuilder()
+            wikitext.set_item_data(data_dict)
+            if data_dict['分类'] != '收藏道具':
+                wikitext.render(data_dict['分类'])
+                wiki.pages.set(path, wikitext.getWikitext())
+        except KeyError:
+            continue
+
+if __name__ == '__main__':
+    try:
+        from config.mywiki import *
+        from config.item_sheet import *
+    except ModuleNotFoundError:
+        print('找不到config')
+        sys.exit()
+
+    try:
+        wiki = dokuwiki.DokuWiki(siteurl, username, password, True)
+    except dokuwiki.DokuWikiError:
+        print('Username or password is wrong ,can\'t access wiki')
+        sys.exit()
+
+    # Set sheet instance
+    item_sheet = GoogleSheets()
+    item_sheet.sheet_id = SPREADSHEET_ID
+    item_sheet.range = RANGE_NAME
+    # pull data
+    item_sheet.pull_data()
+    dict_list = item_sheet.dict_list
+
+    push_item_data(dict_list, wiki)
