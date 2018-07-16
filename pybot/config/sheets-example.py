6d2830fe5eec99c96363615f505255b55b1043be
monado <monado@xenoblade.cn>
2018-07-17 00:04:37 +0800

    update sheets config format

--- pybot/config/sheets-example.py
+++ pybot/config/sheets-example.py
@@ -1,16 +1,16 @@
 # Google Sheets settings, see https://developers.google.com/sheets/api/guides/concepts
 
-sheets = {
-    'enemy': {
-        'SPREADSHEET_ID': '',
-        'RANGE_NAME': ''
-    },
-    'item': {
-        'SPREADSHEET_ID': '',
-        'RANGE_NAME': ''
-    },
-    'shop': {
-        'SPREADSHEET_ID': '',
-        'RANGE_NAME': ''
-    },
+enemy = {
+    'SPREADSHEET_ID': '',
+    'RANGE_NAME': ''
+}
+
+item = {
+    'SPREADSHEET_ID': '',
+    'RANGE_NAME': ''
+}
+
+shop = {
+    'SPREADSHEET_ID': '',
+    'RANGE_NAME': ''
 }
