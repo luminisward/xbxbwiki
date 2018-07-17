ac9919cc6eee974b7a6e0d46b045612f9668eda7
monado <monado@xenoblade.cn>
2018-07-18 00:06:17 +0800

    import page factory

--- pybot/push_data.py
+++ pybot/push_data.py
@@ -1,7 +1,15 @@
 from config.sheets import SHEETS
+from lib.page import Page
 
 def push(table_name):
-    pass
+    data = GoogleSheets(
+        spreadsheet_id=SHEETS[table_name]['SPREADSHEET_ID'],
+        range_name=SHEETS[table_name]['RANGE_NAME']
+        ).get_data().dict_list
+    p = Page().get_page(table_name)
+    p.set_data(data_dict)
+    p.render()
+    dokuwiki.pages.set(path, p.getWikitext())
 
 push_list = []
 for key, value in SHEETS.items():
