10795da3915e4f467dddf9c3906d84b1bd55f73c
monado <monado@xenoblade.cn>
2018-07-19 14:11:42 +0800

    add contract script

--- pybot/lib/page/accessory.py
+++ pybot/lib/page/accessory.py
@@ -17,19 +17,28 @@ class AccessoryPage(Page):
         text = ''
         text += '^分类|{}|\n'.format(self.data['分类'])
         try:
-            text += '^效果说明|{}|\n'.format(self.data['说明'])
+            text += '^效果|{}|\n'.format(self.data['说明'])
         except KeyError:
-            text += '^效果说明|{}|\n'.format(' ')
+            text += '^效果|{}|\n'.format(' ')
         self.wrap_column_half(text)
         self.append_line('</WRAP>')
 
         # 获得方式
         self.build_header(2, '获得方式')
 
+        self.append_line('<WRAP group>')
+
         # 敌人掉落
         text = self.build_header(3, '敌人掉落', ret=True)
         text += '{{backlinks>.#敌人}}\n'
         self.wrap_column_half(text)
+
+        # 商店
+        text = self.build_header(3, '商店', ret=True)
+        text += '{{backlinks>.#商店}}\n'
+        self.wrap_column_half(text)
+
+        self.append_line('</WRAP>')
 
         # 挑战战斗
         text = self.build_header(3, '挑战战斗', ret=True)
