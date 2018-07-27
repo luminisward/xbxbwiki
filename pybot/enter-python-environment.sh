2ea75a931edb69a7c4863e192c9e0c7bddabf384
monado <monado@xenoblade.cn>
2018-07-27 13:50:40 +0800

    mv pybot to xbxbwiki-pybot

--- pybot/enter-python-environment.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -it -v ${PWD}:/root -w /root python:3 $1 $2
--- pybot/lib/factory.py
+++
@@ -1,7 +0,0 @@
-class Factory(object):
-
-    def __init__(self, page_type):
-        self.page_type = page_type
-
-    def create(self):
-        pass
